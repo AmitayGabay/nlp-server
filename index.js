2cbbb127b62f30d2ba94ff8f1f712f72e5a73cc8
michael gabay <michaelgabay770@gmail.com>
2022-07-25 13:27:57 +0300

    aaaa

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.all('*', (req, res, next) => {
     if (!req.get('Origin')) return next();
     res.set('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
-    res.set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,auth-token,x-api-key');
+    res.set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,auth-token,apiKey');
     next();
 });
 routesInit(app);
