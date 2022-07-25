44e5d3b3ab85d0db792af3815ec1e400903afd70
michael gabay <michaelgabay770@gmail.com>
2022-07-25 13:20:28 +0300

    ff

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require("express")
 const path = require("path")
 const http = require("http")
 const routesInit = require("./api/routes/routes-configuration")
-const cors=require('cors')
 const app = express()
 require('./api/db/mongoConnect')
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-cors()
-
 routesInit(app);
 const server = http.createServer(app);
 let port = process.env.PORT || "3000";
-server.listen(port,()=>{console.log("api work")});
+server.listen(port, () => { console.log("api work") });
+
+app.all('*', (req, res, next) => {
+    if (!req.get('Origin')) return next();
+    res.set('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+    res.set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,auth-token,x-api-key');
+    next();
+});
+
 
