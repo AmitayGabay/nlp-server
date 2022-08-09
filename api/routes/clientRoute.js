c24ae3c6f153dc0beffe0fef107f4e078f571d4f
michael gabay <michaelgabay770@gmail.com>
2022-08-10 02:16:21 +0300

    aa

--- api/routes/clientRoute.js
+++ api/routes/clientRoute.js
@@ -1,8 +1,27 @@
-const express= require("express");
+const express = require("express");
+const clientModel = require("../models/clientModel");
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-   return res.json({msg:"clients work"})
+router.get("/", (req, res) => {
+    try {
+        let clients = clientModel.find({})
+        return res.json(clients)
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+router.post("/", async (req, res) => {
+    try {
+        let formData = req.body;
+        await clientModel.create(formData)
+        res.json({ msg: "clientAded" })
+    }
+    catch (err) {
+        console.log(err)
+        res.json(err)
+    }
 })
 
 
